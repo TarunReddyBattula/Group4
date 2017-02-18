434f33932380043060e8d07ba0d1cadb58a6ffa8
vsm9959 <svasireddy@unomaha.edu>
2017-02-17 21:58:28 -0600

    tabs loaded

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,25 @@
 /**
  * Created by San on 2/17/2017.
  */
+// ===============================================================================================================
 function init() {
     var frame = document.getElementById("videoData");
-    frame.width = window.innerWidth * 0.4;
-    frame.height = window.innerHeight * 0.9;
+    frame.width = window.innerWidth * 0.5;
+    frame.height = window.innerHeight ;
 }
 // ============================================================================================================
-function initGoogle() {
+function initSearch() {
     var frame = document.getElementById("videoData");
-    frame.src = "https://www.bing.com";
+    frame.src = "https://duckduckgo.com/";
 }
+// ============================================================================================================
+function initWiki() {
+    var frame = document.getElementById("videoData");
+    frame.src = "https://www.wikipedia.org/";
+}
+// ============================================================================================================
+function initData() {
+    var frame = document.getElementById("videoData");
+    frame.src = "https://duckduckgo.com/";
+}
+// ===============================================================================================================
