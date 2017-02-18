daad128c365f4fa8ff8f7fe8f42fb1a9bdb4d463
vsm9959 <svasireddy@unomaha.edu>
2017-02-17 21:49:05 -0600

    screen 2 template

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+/**
+ * Created by San on 2/17/2017.
+ */
+function init() {
+    var frame = document.getElementById("videoData");
+    frame.width = window.innerWidth * 0.4;
+    frame.height = window.innerHeight * 0.9;
+}
+// ============================================================================================================
+function initGoogle() {
+    var frame = document.getElementById("videoData");
+    frame.src = "https://www.bing.com";
+}
